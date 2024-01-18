c9e1f5af246b115afa73ba5cba4b8763a97031ef
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-01-18 16:58:09 +0400

    first commit

---
+++ config/accordionsConfig.js
@@ -0,0 +1,21 @@
+const accordionsConfig = [
+  {
+    id: 1,
+    title: "Accordion Title",
+    content: "Accordion Content",
+    isOpen: false,
+  },
+  {
+    id: 2,
+    title: "Accordion Title",
+    content: "Accordion Content",
+    isOpen: false,
+  },{
+    id: 3,
+    title: "Accordion Title",
+    content: "Accordion Content",
+    isOpen: false,
+  },
+];
+
+export default accordionsConfig;
---
+++ config/coursesConfig.js
@@ -0,0 +1,58 @@
+const coursesConfig = [
+  {
+    id: 1,
+    title: "Course 1",
+    image: "path/to/course1-image.jpg",
+    description: "Description for Course 1",
+  },
+  {
+    id: 2,
+    title: "Course 2",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 3,
+    title: "Course 3",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 4,
+    title: "Course 4",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 5,
+    title: "Course 5",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 6,
+    title: "Course 6",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 7,
+    title: "Course 7",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 8,
+    title: "Course 8",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+  {
+    id: 9,
+    title: "Course 9",
+    image: "path/to/course2-image.jpg",
+    description: "Description for Course 2",
+  },
+];
+
+export default coursesConfig;
