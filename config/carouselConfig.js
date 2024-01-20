35fffe1a8f58c48f24befc85133ec0344ed2a5ca
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-01-20 19:40:00 +0400

    chore: add carousel config

---
+++ config/carouselConfig.js
@@ -0,0 +1,32 @@
+const carouselConfig = [
+  {
+    id: 1,
+    image: "../assets/images/carousel/usaidCarousel.webp",
+  },
+  {
+    id: 2,
+    image: "../assets/images/carousel/spaceCarousel.webp",
+  },
+  {
+    id: 3,
+    image: "../assets/images/carousel/tntCarousel.webp",
+  },
+  {
+    id: 4,
+    image: "../assets/images/carousel/tegetaCarousel.webp",
+  },
+  {
+    id: 5,
+    image: "../assets/images/carousel/spectreCarousel.webp",
+  },
+  {
+    id: 6,
+    image: "../assets/images/carousel/tbcCarousel.webp",
+  },
+  {
+    id: 7,
+    image: "../assets/images/carousel/ufcCarousel.webp",
+  },
+];
+
+export default carouselConfig;
