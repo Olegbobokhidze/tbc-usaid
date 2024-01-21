74abeac04a329aa5ad1c2012500fe92882c56233
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-01-21 22:24:24 +0400

    feat: add accordion

---
+++ components/accordion/accordion.js
@@ -0,0 +1,66 @@
+import accordionsConfig from "../../config/accordionsConfig.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accordionsContainer = document.querySelector(".accordions");
+
+  accordionsConfig.forEach((accordion, index) => {
+    accordion.isOpen = false;
+
+    const accordionElement = document.createElement("div");
+    accordionElement.classList.add("accordion");
+
+    const questionElement = document.createElement("div");
+    questionElement.classList.add("accordionQuestion");
+    questionElement.innerHTML = accordion.question;
+
+    const answersElement = document.createElement("div");
+    answersElement.classList.add("accordionAnswers");
+    answersElement.style.display = "none";
+
+    if (accordion.header) {
+      const headerElement = document.createElement("div");
+      headerElement.classList.add("accordionHeader");
+      headerElement.innerHTML = accordion.header;
+      answersElement.appendChild(headerElement);
+    }
+
+    if (accordion.stages) {
+      const stagesElement = document.createElement("div");
+      stagesElement.classList.add("accordionStage");
+      accordion.stages.forEach((stage) => {
+        const stageParagraph = document.createElement("p");
+        stageParagraph.innerHTML = stage;
+        stagesElement.appendChild(stageParagraph);
+      });
+      answersElement.appendChild(stagesElement);
+    }
+
+    const answerParagraph = document.createElement("p");
+    answerParagraph.innerHTML = accordion.answer;
+    answersElement.appendChild(answerParagraph);
+
+    questionElement.addEventListener("click", () => {
+      accordion.isOpen = !accordion.isOpen;
+
+      accordionsConfig.forEach((acc) => {
+        if (acc !== accordion) {
+          acc.isOpen = false;
+          const accIndex = accordionsConfig.indexOf(acc);
+          const accElement =
+            accordionsContainer.children[accIndex].querySelector(
+              ".accordionAnswers"
+            );
+          accElement.style.display = "none";
+        }
+      });
+
+      const answerElement =
+        accordionsContainer.children[index].querySelector(".accordionAnswers");
+      answerElement.style.display = accordion.isOpen ? "flex" : "none";
+    });
+
+    accordionElement.appendChild(questionElement);
+    accordionElement.appendChild(answersElement);
+    accordionsContainer.appendChild(accordionElement);
+  });
+});
